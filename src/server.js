2fff144d7ecc5a567016bdd560be6050546e19ff
fabio <fabio>
2024-10-28 18:18:36 -0300

    CRUD Completo

--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@ const app = express()
 process.loadEnvFile()
 const port = process.env.PORT || 3000
 const { connectToMongoDB, disconnectFromMongoDB } = require('./config/mongoDB.js')
-const { ObjectId } = require('mongodb')
+const { ObjectId, ReturnDocument } = require('mongodb')
 
+// Middleware para parsear JSON
+app.use(express.json())
 //Middleware para acceder a la BD y la Collection
 app.use(async (req, res, next) => {
     const client = await connectToMongoDB()
@@ -28,12 +30,65 @@ app.get('/movies', async (req, res) => {
 })
 app.get('/movies/:id', async (req, res) => {
     const movie = await req.movies.findOne({ _id: new ObjectId(req.params.id) })
-    res.json(movie)
+    if (!movie)
+        res.status(404).send('No se encontro la pelicula')
+    else 
+        res.json(movie)
 })
 app.get('/movies/genre/:genre', async (req, res) => {
     const { genre } = req.params
     const movies = await req.movies.find({ genre: {$in: [genre]} }).toArray()
     res.json(movies)
+})
+
+// Agregar una pelicula
+app.post('/movies/', async (req,res) => {
+    const nuevaPeli = req.body
+    if (nuevaPeli === undefined)
+        res.status(400).send('Error en el formato de la peli')
+    try {
+        await req.movies.insertOne(nuevaPeli)
+        res.status(201).send(nuevaPeli)
+    } catch (error) {
+        res.status(500).send('Error al agregar pelicula')
+    }
+})
+
+// modificar una pelicula
+app.put('/movies/:id', async (req,res) => {
+    const datosPeli = req.body
+    const id = new ObjectId(req.params.id)
+
+    if (!datosPeli)
+        res.status(400).send('Error en el formato de los nuevos datos de la peli')
+
+    try {
+        peliActualizada = await req.movies.findOneAndUpdate(
+            {_id:id}, 
+            {$set: datosPeli},
+            {ReturnDocument: 'after'}
+        )
+        if (!peliActualizada)
+            res.status(404).send('Error al encontrar la pelicula a actualizar')
+        else 
+            res.status(201).send(peliActualizada)
+    } catch (error) {
+        res.status(500).send('Error al agregar pelicula')
+    }
+})
+
+app.delete('/movies/:id', async (req, res) => {
+    try {
+        const resultado = await req.movies.deleteOne({ _id: new ObjectId(req.params.id) })
+        if (resultado.deletedCount > 0){
+            res.status(204).send('Pelicula borrada exitosamente')
+        }else {
+            res.status(404).send('No se encontro la pelicula a borrar')
+        }
+    } catch (error) {
+        res.status(500).send('Error al agregar pelicula')
+    }
+
 })
 
 app.listen(port, () => {
